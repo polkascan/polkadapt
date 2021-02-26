2b21d317efb62f28ac3334faf2f524a5ddc04596
michielvandergraaf <michiel@kriekkop.nl>
2021-02-26 22:07:48 +0100

    Polkascan adapter call functions updated

--- projects/polkascan/src/lib/web-socket/block.functions.ts
+++ projects/polkascan/src/lib/web-socket/block.functions.ts
@@ -3,24 +3,31 @@ import { Block } from '../polkascan.types';
 
 const genericBlockFields = 'id, hash, parentHash, stateRoot, extrinsicsRoot, countExtrinsics, countEvents';
 
+const isBlockHash = (hash: any): boolean => {
+  return (typeof hash === 'string' || (hash as any) instanceof String) && hash.startsWith('0x');
+};
+
+const isBlockNumber = (nr: any): boolean => {
+  return Number.isInteger(nr) && nr >= 0;
+};
+
+
 export const getBlock = (adapter: Adapter) => {
-  return async (hashOrNumber: string | number): Promise<Block> => {
-    let filter: string;
-    if (typeof hashOrNumber === 'string') {
+  return async (hashOrNumber?: string | number): Promise<Block> => {
+    let filter;
+    if (isBlockHash(hashOrNumber)) {
       // Fetch specific block;
-      filter = `(filters: { hash: "${hashOrNumber}" })`;
-    } else if (Number.isInteger(hashOrNumber)) {
-      filter = `(filters: { id: ${hashOrNumber} })`;
-    } else if (hashOrNumber !== undefined) {
-      // hashOrNumber is defined but is not a string or integer.
-      throw new Error('[PolkascanAdapter] getBlocks: Hash must be a string or Number must be a number.');
+      filter = `filters: { hashEq: "${hashOrNumber}" }`;
+    } else if (isBlockNumber(hashOrNumber)) {
+      filter = `filters: { id: ${hashOrNumber} }`;
     }
 
-    const query = `query { getBlock${filter || ''} { ${genericBlockFields} } }`;
+    const query = `query { getBlock${filter ? `(${filter})` : ''} { ${genericBlockFields} } }`;
+
     try {
       const result = await adapter.socket.query(query);
       const block = result.getBlock;
-      block.number = block.id; // Fix when backend contains number as attribute
+      block.number = parseInt(block.id as any, 10); // Fix when backend contains number as attribute
       return block;
     } catch (e) {
       throw new Error(e);
@@ -32,31 +39,23 @@ export const getBlock = (adapter: Adapter) => {
 export const getBlocksFrom = (adapter: Adapter) => {
   return async (hashOrNumber: string | number, pageSize?: number, pageNr?: number): Promise<Block[]> => {
     // TODO FIX PAGE SIZE, PAGE NR.
-    const queryArgs: any = {};
-    if (hashOrNumber) {
-      // Fetch specific block;
-      queryArgs.filters = queryArgs.filters || {};
-      if (typeof hashOrNumber === 'string') {
-        queryArgs.filters.hashFrom = hashOrNumber;
-      } else if (Number.isInteger(hashOrNumber)) {
-        queryArgs.filters.idGte = hashOrNumber;
-      } else {
-        throw new Error('[PolkascanAdapter] getBlocksFrom: Hash must be a string or Number must be a number.');
-      }
+    let filter = '';
+
+    if (isBlockHash(hashOrNumber)) {
+      filter = `filters: { hashFrom: "${hashOrNumber}" }`;
+    } else if (isBlockNumber(hashOrNumber)) {
+      filter = `filters: { idGte: ${hashOrNumber} }`;
+    } else {
+      throw new Error('[PolkascanAdapter] getBlocksFrom: Supplied hashOrNumber must be of type string or number.');
     }
 
-    const queryArgsString = Object.keys(queryArgs).length > 0
-      ? `( ${JSON.stringify(queryArgs)} )`
-      : '';
-
-    const query =
-      `query { getBlocks${queryArgsString} { ${genericBlockFields} } }`;
+    const query = `query {getBlocks( ${filter}) { ${genericBlockFields} } }`;
 
     try {
       // @ts-ignore
       const result = await adapter.socket.query(query);
       const blocks: Block[] = result.getBlocks;
-      blocks.forEach((block) => block.number = block.id); // Fix when backend contains number as attribute.
+      blocks.forEach((block) => block.number = parseInt(block.id as any, 10)); // Fix when backend contains number as attribute.
       return blocks;
     } catch (e) {
       throw new Error(e);
@@ -68,30 +67,24 @@ export const getBlocksFrom = (adapter: Adapter) => {
 export const getBlocksUntil = (adapter: Adapter) => {
   return async (hashOrNumber: string | number, pageSize?: number, pageNr?: number): Promise<Block[]> => {
     // TODO FIX PAGE SIZE, PAGE NR.
-    const queryArgs: any = {};
-    if (hashOrNumber) {
-      // Fetch specific block;
-      queryArgs.filters = queryArgs.filters || {};
-      if (typeof hashOrNumber === 'string') {
-        queryArgs.filters.hashUntil = hashOrNumber;
-      } else if (Number.isInteger(hashOrNumber)) {
-        queryArgs.filters.idLte = hashOrNumber;
-      } else {
-        throw new Error('[PolkascanAdapter] getBlocksUntil: Hash must be a string or Number must be a number.');
-      }
+    let filter = '';
+
+    // Fetch specific block;
+    if (isBlockHash(hashOrNumber)) {
+      filter = `filters: { hashUntil: "${hashOrNumber}" }`;
+    } else if (isBlockNumber(hashOrNumber)) {
+      filter = `filters: { idLte: ${hashOrNumber} }`;
+    } else {
+      throw new Error('[PolkascanAdapter] getBlocksUntil: Supplied hashOrNumber must be of type string or number.');
     }
 
-    const queryArgsString = Object.keys(queryArgs).length > 0
-      ? `( ${JSON.stringify(queryArgs)} )`
-      : '';
-
     const query =
-      `query { getBlocks${queryArgsString} { ${genericBlockFields} } }`;
+      `query { getBlocks(${filter}) { ${genericBlockFields} } }`;
 
     try {
       const result = await adapter.socket.query(query);
       const blocks: Block[] = result.getBlocks;
-      blocks.forEach((block) => block.number = block.id); // Fix when backend contains number as attribute.
+      blocks.forEach((block) => block.number = parseInt(block.id as any, 10)); // Fix when backend contains number as attribute.
       return blocks;
     } catch (e) {
       throw new Error(e);
@@ -107,7 +100,7 @@ export const subscribeFinalizedBlocks = (adapter: Adapter) => {
     return await adapter.socket.createSubscription(query, (result) => {
       try {
         const block = result.block;
-        block.number = block.id; // Fix when backend contains number as attribute
+        block.number = parseInt(block.id as any, 10); // Fix when backend contains number as attribute
         callback(block);
       } catch (e) {
         // Ignore.
@@ -119,15 +112,17 @@ export const subscribeFinalizedBlocks = (adapter: Adapter) => {
 
 export const getBlockAugmentation = (adapter: Adapter) => {
   return async (hash: string): Promise<any> => {
-    if (typeof hash !== 'string') {
-      throw new Error('[PolkascanAdapter] getBlock (augmentation): Hash must be a string.');
+    if (!isBlockHash(hash)) {
+      throw new Error('[PolkascanAdapter] getBlock (augmentation): Hash must be of type string.');
     }
 
     // Get data from polkascan to augment it to the rpc block.
     const query = `query { getBlock(filters: { hash: "${hash}" }) { id, countExtrinsics, countEvents } }`;
     try {
       const result = await adapter.socket.query(query);
-      return result.getBlock;
+      const block = result.getBlock;
+      block.number = parseInt(block.id as any, 10);
+      return {block};
     } catch (e) {
       // Ignore failure. We won't augment the block into the rpc fetched block;
       return {};
